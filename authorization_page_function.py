d945e8af19bebad6d5fea9bbb7658eb6fef35427
Annddreyy <andreymail22112006@gmail.com>
2024-10-04 16:33:20 +0300

    Fix authorization page

--- authorization_page_function.py
+++ authorization_page_function.py
@@ -9,23 +9,24 @@ authorization_page_blueprint = Blueprint('authorization_page', __name__)
 
 @authorization_page_blueprint.route('/authorization', methods=['GET', 'POST'])
 def authorization_page():
-    if request.method == 'POST':
-        login = request.form.get('login')
-        password = request.form.get('password')
-        result = hashlib.sha256(password.encode()).hexdigest()
+    if 'user_id' not in session:
+        if request.method == 'POST':
+            login = request.form.get('login')
+            password = request.form.get('password')
+            result = hashlib.sha256(password.encode()).hexdigest()
 
-        response = requests.get(f'{BASE_URL}/authorization').json()
+            response = requests.get(f'{BASE_URL}/authorization').json()
 
-        for user in response:
-            if user['login'] == login and user['password'] == result:
-                session['user_id'] = user['id']
+            for user in response:
+                if user['login'] == login and user['password'] == result:
+                    session['user_id'] = user['id']
 
-                response = requests.get(f'{BASE_URL}/clients/{user['id']}').json()
+                    response = requests.get(f'{BASE_URL}/clients/{user['id']}').json()
 
-                session['is_admin'] = response[0]['role'] == 2
+                    session['is_admin'] = response[0]['role'] == 2
 
-                return redirect(url_for('main_page.main_page'))
+                    return redirect(url_for('main_page.main_page'))
 
-        return redirect(url_for('authorization_page.authorization_page')), flash('Неверный логин или пароль!')
-
-    return render_template('authorization.html')
+            return redirect(url_for('authorization_page.authorization_page')), flash('Неверный логин или пароль!')
+        return render_template('authorization.html')
+    return redirect(url_for('main_page.main_page'))
