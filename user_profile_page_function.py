fc0d5364986b1629043de6826046a7549dabfc15
Annddreyy <andreymail22112006@gmail.com>
2024-10-04 19:03:42 +0300

    Add profile page

---
+++ user_profile_page_function.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, session
+
+from search_information import news_search, get_top_bar_information
+
+user_profile_page_blueprint = Blueprint('user_profile_page', __name__)
+
+@user_profile_page_blueprint.route('/user-profile')
+def personal_page():
+    all_news = news_search()
+
+    user_information = get_top_bar_information(session['user_id'])
+
+    return render_template(
+        'user-profile.html',
+        all_news=all_news,
+        user_information=user_information,
+        is_admin=session['is_admin']
+    )
