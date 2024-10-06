9170be089680196a5dc2a2a7cb9b4d206d4fe25d
Annddreyy <andreymail22112006@gmail.com>
2024-10-06 21:30:33 +0300

    Fix resume page

--- resume_page_function.py
+++ resume_page_function.py
@@ -1,18 +1,32 @@
 import requests
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, session, url_for
+from werkzeug.utils import redirect
 
 from env_variables import BASE_URL
 from search_information import news_search, get_jobs, get_resumes
 
 resume_page_blueprint = Blueprint('resume_page', __name__)
 
-@resume_page_blueprint.route('/resume')
+@resume_page_blueprint.route('/resume', methods=['GET', 'POST'])
 def resume_page():
     all_resumes = get_resumes()
     all_news = news_search()
     all_jobs = get_jobs()
 
-    print(all_jobs)
+    if request.method == 'POST':
+        job_id = request.form.get('job-title')
+        response = requests.get(f'{BASE_URL}/jobs/{job_id}').json()['title']
+        new_resumes = []
+        for resume in all_resumes:
+            if resume[2] == response:
+                new_resumes.append(resume)
+
+        session['resume'] = new_resumes
+
+        return redirect(url_for('resume_page.resume_page'))
+
+    if 'resume' in session:
+        all_resumes = session['resume']
 
     return render_template(
         'resume.html',
@@ -20,3 +34,9 @@ def resume_page():
         all_news=all_news,
         all_jobs = all_jobs
     )
+
+@resume_page_blueprint.route('/reset_resume_filter', methods=['GET'])
+def reset_resume_filter():
+    if 'resume' in session:
+        session.pop('resume')
+    return redirect(url_for('resume_page.resume_page'))
